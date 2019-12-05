348ee033518bf75c7f37db3d1ccabc5aa9fe0d2e
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-05 23:37:36 +1100

    Route function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
   );
 }
 
