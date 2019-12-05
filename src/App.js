df888b8fdf004b2ce937f0a9ccd6d31827b24adb
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-05 23:50:35 +1100

    3 pages added with css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
