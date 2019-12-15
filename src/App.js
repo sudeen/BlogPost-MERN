91f728e62b8096a42e15dcc35a342c7c7e393941
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-15 23:26:21 +1100

    URL parameter with react router

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
           <Route path="/articles-list" component={ArticlesList} />
-          <Route path="/article" component={ArticlePage} />
+          <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
     </Router>
