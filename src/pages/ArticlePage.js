d883e717a25c98690bff3f6ccfd0a957d6ada0ef
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-15 23:34:41 +1100

    linking the articles

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import articleContent from "./article-content";
+
+const ArticlePage = ({match}) => {
+  const name = match.params.name; 
+  const article = articleContent.find(article => article.name === name);
+  if (!article) return <h1>Article does not exist</h1>
+  return (
+    <>
+<h1>{article.title}</h1>
+{article.content.map((paragraph, key) => (
+  <p key={key}>{paragraph}</p>
+))}
+  </>
+);
+}
+
+export default ArticlePage;
