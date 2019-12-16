b20a4103e839b94528f5a6487c659e2876342305
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-16 23:57:54 +1100

    displayed comments in each articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NonFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
 
 const ArticlePage = ({match}) => {
 
-
   const name = match.params.name; 
   const article = articleContent.find(article => article.name === name);
+  
+  const [articleInfo, setArticleInfo] = useState({upvotes: 0 , comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
   if (!article) return <NonFoundPage />
 
   const otherArticles = articleContent.filter(article => article.name !== name);
@@ -15,9 +27,11 @@ const ArticlePage = ({match}) => {
   return (
     <>
   <h1>{article.title}</h1>
+  <p>This post has been upvoted {articleInfo.upvotes} times</p>
   {article.content.map((paragraph, key) => (
     <p key={key}>{paragraph}</p>
   ))}
+  <CommentsList comments={articleInfo.comments} />
   <h3>Other Articles</h3>
   <ArticlesList articles={otherArticles}/>
   </>
