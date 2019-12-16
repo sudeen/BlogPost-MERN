e97246851dfe2a9e4bfd3477236e4b5ceeb3e857
sudeen <ranjitkarsudeen14@gmail.com>
2019-12-17 00:07:24 +1100

    added upvote button feature

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NonFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
+import UpvotesSection from "../components/UpvotesSection";
 
 const ArticlePage = ({match}) => {
 
@@ -27,7 +28,7 @@ const ArticlePage = ({match}) => {
   return (
     <>
   <h1>{article.title}</h1>
-  <p>This post has been upvoted {articleInfo.upvotes} times</p>
+  <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
   {article.content.map((paragraph, key) => (
     <p key={key}>{paragraph}</p>
   ))}
